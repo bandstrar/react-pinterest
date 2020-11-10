f0a7bf037a0f9224f5d21c03d03a10520d379228
bandstrar <rob.bandstra@gmail.com>
2020-11-09 20:08:45 -0600

    moved folders

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import firebase from 'firebase/app';
-import fbConnection from './helpers/data/connection';
+import fbConnection from '../helpers/data/connection';
 import './App.scss';
 import Auth from '../components/Auth';
 import MyNavbar from '../components/MyNavbar';
